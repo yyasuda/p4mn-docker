dd68686218e66a5bf416b3b597e49195ee0534e1
Yutaka Yasuda <yasuda@cc.kyoto-su.ac.jp>
2025-09-05 10:20:15 +0900

    added IPv6 disable feature to remove ICMP6 broadcast

--- run-p4mn.sh
+++ run-p4mn.sh
@@ -13,6 +13,20 @@ if [[ "${PKTDUMP}" == "true" ]]; then
   SWITCH_OPTS+=",pktdump=true"
 fi
 
+# IPv6 on/off option. use -e IPV6=false to disable IPv6.
+IPV6="${IPV6:-true}"
+if [[ "${IPV6}" == "false" ]]; then
+    # ===== IPv6 off in root ns (and defaults for future namespaces) =====
+    sysctl -q -w net.ipv6.conf.all.disable_ipv6=1
+    sysctl -q -w net.ipv6.conf.default.disable_ipv6=1
+    sysctl -q -w net.ipv6.conf.lo.disable_ipv6=1
+    # RA/autoconf disabled (just in case) 
+    sysctl -q -w net.ipv6.conf.all.accept_ra=0
+    sysctl -q -w net.ipv6.conf.default.accept_ra=0
+    sysctl -q -w net.ipv6.conf.all.autoconf=0
+    sysctl -q -w net.ipv6.conf.default.autoconf=0
+fi
+
 # "$@" is additonal argument of docker run, pass it to "mn".
 exec mn \
   --custom /root/bmv2.py \
